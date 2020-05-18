0120936fc402514627ba064a109791ebd5398d65
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-05-18 21:20:16 +0300

    Struct

---
+++ IntroductionToOOP/IntroductionToOOP/Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+/*
+struct Point {
+
+	double x;
+	double y;
+};
+*/
+class Point{
+
+   double x;
+   double y;
+};
+
+void main() {
+	Point A;
+	
+	A.x = 2;
+	A.y = 3;
+	Point* pa = &A;
+	cout << pa->x << "\t" << pa->y << endl;
+	cout << A.x << "\t" << A.y << endl;
+
+}
