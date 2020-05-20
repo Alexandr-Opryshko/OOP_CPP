1ed89dc7bf92317c257bf3b277c77a0f424bd54e
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-05-20 23:03:59 +0300

    Start

--- IntroductionToOOP/IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/IntroductionToOOP/Source.cpp
@@ -1,25 +1,47 @@
 #include<iostream>
 using namespace std;
-/*
-struct Point {
 
-	double x;
-	double y;
-};
-*/
+
 class Point{
-public:
+
    double x;
    double y;
+public:
+	double get_x() const{
+		return x;
+	}
+	double get_y() const{
+		return y;
+	}
+	void set_x(double x) {
+		this->x = x;
+	}
+	void set_y(double y) {
+		this->y = y;
+	}
+};
+class Point3D :public Point {
+	double z;
+public:
+	double get_z() const {
+		return z;
+	}
+	void set_z(double z) {
+		this->z = z;
+	}
 };
 
 void main() {
+		
 	Point A;
-	
-	A.x = 2;
-	A.y = 3;
-	Point* pa = &A;
-	cout << pa->x << "\t" << pa->y << endl;
-	cout << A.x << "\t" << A.y << endl;
+	Point3D B;
 
+	A.set_x(2);
+	A.set_y(3);
+	B.set_z(20);
+	
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	Point* pA = &A;
+	cout << pA->get_x() << "\t" << pA->get_y() << endl;
+	cout << A.get_x() << "\t" << A.get_y() << "\t" << B.get_z() << endl;
 }
