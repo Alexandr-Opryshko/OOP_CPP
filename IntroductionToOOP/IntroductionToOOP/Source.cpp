08e958c91a31ecd3292ada6282e69a67bb5ed083
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-05-21 09:38:18 +0300

    Distance

--- IntroductionToOOP/IntroductionToOOP/Source.cpp
+++ IntroductionToOOP/IntroductionToOOP/Source.cpp
@@ -1,47 +1,95 @@
 #include<iostream>
 using namespace std;
 
+#include <iostream>     // std::cout
+#include <iterator>     // std::distance
+#include <list>         // std::list
 
-class Point{
-
-   double x;
-   double y;
+class Distance {
 public:
-	double get_x() const{
+	void PrintDisplay(int x, int y) {
+		x = set_X(x);
+		y = set_Y(y);
+		std::cout << " ";
+		for (int i = 0; i < this->x_end;i++) {
+			std::cout << "+";
+		}
+		std::cout << std::endl;
+		for (int i = 0; i < this->y_end;i++) {
+			std::cout << "+";
+			for (int j = 0; j < this->x_end;j++) {
+				if ((i == y) && (j == x)) {
+					std::cout << "*";
+				}
+				else std::cout << " ";
+			}
+			std::cout << "+" << std::endl;
+		}
+		std::cout << " ";
+		for (int i = 0; i < this->x_end;i++) {
+			std::cout << "+";
+		}
+		std::cout << std::endl;
+		return;
+	}
+	int get_X()const {
 		return x;
 	}
-	double get_y() const{
+	int set_X(int x) {
+		x = ErrorInputX(x);
+		this->x = x;
+		return x;
+	}
+	int get_X_End()const {
+		return this->x_end;
+	}
+	int get_Y()const {
 		return y;
 	}
-	void set_x(double x) {
-		this->x = x;
-	}
-	void set_y(double y) {
+	int set_Y(int y) {
+		y = ErrorInputY(y);
 		this->y = y;
+		return y;
 	}
-};
-class Point3D :public Point {
-	double z;
-public:
-	double get_z() const {
-		return z;
+	int get_Y_End()const {
+		return this->y_end;
 	}
-	void set_z(double z) {
-		this->z = z;
+private:
+	int x = 0;
+	int y = 0;
+	const int x_end = 30;
+	const int y_end = 20;
+	int ErrorInputX(int x) {
+		if (x > x_end) return x_end - 1;
+		else return x;
+	}
+	int ErrorInputY(int y) {
+		if (y > y_end) return y_end - 1;
+		else return y;
 	}
 };
 
 void main() {
-		
-	Point A;
-	Point3D B;
+	setlocale(LC_ALL, "");
+	Distance D;
+	int x = 0;
+	int y = 0;
+	std::cout << "Введите ваше расположение по оси x - "; std::cin >> x; std::cout << std::endl;
+	std::cout << "Введите ваше расположение по оси y - "; std::cin >> y; std::cout << std::endl;
 
-	A.set_x(2);
-	A.set_y(3);
-	B.set_z(20);
+	D.PrintDisplay(x, y);
+
+	x = D.get_X_End() - x;
+	y = D.get_Y_End() - y;
+	if ((x < 0) || (y < 0)) {
+		std::cout << "Вы вышли за границы оси координат:"<< std::endl;
+		std::cout << "\t x = " << x << "\t y = " << y << std::endl;
+	}
+	else {
+		std::cout << "Остаток до границы по оси x - " << x << std::endl;
+		std::cout << "Остаток до границы по оси y - " << y << std::endl;
+	}
 	
-	cout << A.get_x() << "\t" << A.get_y() << endl;
-	Point* pA = &A;
-	cout << pA->get_x() << "\t" << pA->get_y() << endl;
-	cout << A.get_x() << "\t" << A.get_y() << "\t" << B.get_z() << endl;
+
+
 }
