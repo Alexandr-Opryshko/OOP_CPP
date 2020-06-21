176682915248753f00c4b1bbd369e6c276181a6a
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-06-21 13:26:08 +0300

    --

--- IntroductionToOOP/String/String.cpp
+++ IntroductionToOOP/String/String.cpp
@@ -8,4 +8,13 @@ void main() {
 	str.print();
 	String str1 = "Hello";
 	str1.print();
+
+
+	//	String str2 = str1;
+	//	str2.print();
+	/*
+		cout << str2 << endl;
+		str = str1;
+		cout << str1 << endl;
+		*/
 }
--- IntroductionToOOP/String/String.h
+++ IntroductionToOOP/String/String.h
@@ -6,6 +6,33 @@ class String {
     int size;
     char* str;
 public:
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="valueStr"></param>
+	void Set_str(const char* valueStr) {
+		this->size = strlen(valueStr) + 1;
+		delete[] this->str;
+		this->str = new char[size];
+		for (int i = 0; i < this->size; i++) {
+			this->str[i] = valueStr[i];
+		}
+	}
+	/// <summary> получить длину строки </summary>
+	/// <returns></returns>
+	const int Get_size() {
+		return this->size;
+	}
+	/// <summary> получить строковое значени </summary>
+	/// <returns></returns>
+	const char& Get_str() {
+		return *str;
+	}
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="size"></param>
+    /// <returns></returns>
     explicit String(int size = 80) {
         this->size = size;
         this->str = new char[size] {};
