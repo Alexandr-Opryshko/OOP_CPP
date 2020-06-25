5df2bb9778629ec61a3b0a3d6b2711be1fcfb19e
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-06-25 15:17:28 +0300

    Move constructor

--- IntroductionToOOP/String/String.cpp
+++ IntroductionToOOP/String/String.cpp
@@ -16,21 +16,33 @@ void main() {
 
 	String str(8);
 	str.print();
-	String str1 = "Hello! ";
+	String str1 = "Hello!";
 	str1.print();
 
 
-		String str2 = str1;
-		cout << str2 << endl;
-		str2 = str;
-		cout << str1 << endl;
-		str2 = "What is your name? ";
-		str = "Good by. ";
-		cout << str << endl;
-		
-		str = str1 + str2 + str;
-		cout << str << endl;
-		str += str;
-		cout << str << endl;
+	String str2 = str1;
+	cout << str2 << endl;
+	str2 = str;
+	cout << str1 << endl;
+	str2 = "What is your name?";
+	cout << str2 << endl;
+	str = "Good by.";
+	cout << str << endl;
+	cout << str.Get_str() << " " << str.Get_str() << endl;
 
+	str = str1 + str2 + str;
+	cout << str << endl;
+	str += str;
+	cout << str << endl;
+
+	String A("Hello!");
+	A.print();
+	String C = String("Hello");
+	C.print();
+	String B = A;
+	B.print();
+	B = C;
+	B.print();
+	A = A + B;
+	A.print();
 }
--- IntroductionToOOP/String/String.h
+++ IntroductionToOOP/String/String.h
@@ -6,31 +6,38 @@ class String {
     int size;
     char* str;
 public:
-	/// <summary>
-	/// 
-	/// </summary>
+	/// <summary>  </summary>
 	/// <param name="valueStr"></param>
 	void Set_str(const char* valueStr) {
 		this->size = (int)strlen(valueStr) + 1;
 		delete[] this->str;
 		this->str = new char[size];
-		for (int i = 0; i < this->size; i++) {
+		for (int i = 0; i < this->size-1; i++) {
 			this->str[i] = valueStr[i];
 		}
 	}
-	/// <summary> получить длину строки </summary>
+	/// <summary> получить длину строки константный метод</summary>
 	/// <returns></returns>
 	const int Get_size() const{
 		return this->size;
 	}
-	/// <summary> получить строковое значени </summary>
+	/// <summary> получить длину строки </summary>
+	/// <returns></returns>
+	int Get_size() {
+		return this->size;
+	}
+	/// <summary> получить строковое значени константный метод </summary>
 	/// <returns></returns>
 	const char* Get_str() const{
 		return str;
 	}
-    /// <summary>
-    /// 
-    /// </summary>
+	/// <summary> получить строковое значени </summary>
+	/// <returns></returns>
+	char* Get_str() {
+		return str;
+	}
+
+    /// <summary>  </summary>
     /// <param name="size"></param>
     /// <returns></returns>
     explicit String(int size = 80) {
@@ -38,30 +45,43 @@ public:
         this->str = new char[size] {};
         cout << "DefConstructor:\t\t" << this << endl;
     }
+
 	String(const char* arrayChar) {
-//		this->str = nullptr;
 		this->size = (int)strlen(arrayChar) + 1;
 		this->str = new char[size] {};
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < this->size-1; i++) {
 			this->str[i] = arrayChar[i];
 		}
 		std::cout << "Constructor:\t\t" << this << std::endl;
 	}
+	
+	String(const char& other) : str(new char(other)) {
+		this->size = (int)strlen(str) + 1;
+		std::cout << "Constructor:  ?\t\t" << this << std::endl;
+	}
 
+	String(String&& other) : str(other.str), size(other.size) {
+		other.str = nullptr;
+		std::cout << "MoveConstructor:\t" << this << std::endl;
+	}
+	/// <summary> конструктор копирования </summary>
+	/// <param name="other"></param>
+	/// <returns></returns>
 	String(const String& other) {
 		this->size = other.size;
 		this->str = new char[this->size]{};
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < this->size - 1; i++) {
 			this->str[i] = other.str[i];
 		}
 		cout << "CopyConstructor:\t" << this << endl;
 	}
+	// деструктор
     ~String() {
         delete[] str;
         cout << "Destructor:\t\t" << this << endl;
     }
 // --- Operators ---
-	String& operator=(const String& other) {
+	String& operator= (const String& other) {
 		if (this->size != other.size) {
 			this->size = other.size;
 			delete[] this->str;
@@ -73,6 +93,16 @@ public:
 		cout << "AssignmentOperator\t" << this << endl;
 		return *this;
 	}
+	// move assignment
+	String& operator= (String&& other) {
+		delete str;
+		this->size = other.size;
+		this->str = other.str;
+		other.str = nullptr;
+		cout << "MoveAssignment\t\t" << this << endl;
+		return *this;
+	}
+
 	String operator+(const String& other) const{
 		int size = this->size + other.size -1;
 		String temp(this->size + other.size - 1);
@@ -82,6 +112,17 @@ public:
 		cout << "SumOperator\t\t" << this << endl;
 		return temp;
 	}
+
+	
+	//// access content:
+	//const char& content() const { 
+	//	return *str; 
+	//}
+	//// addition:
+	//String operator+(const String& other) {
+	//	return String(content() + other.content());
+	//}
+
 	String& operator+=(const String& other) {
 		int size = this->size + other.size - 1;
 		String temp = *this;
