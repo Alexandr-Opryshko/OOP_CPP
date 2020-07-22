12981892720d707d978b8bb84da2bca4194cc231
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-07-22 22:25:12 +0300

    LIST end

--- ForwardList/ForwardList/Source.cpp
+++ ForwardList/ForwardList/Source.cpp
@@ -46,6 +46,66 @@ public:
 		}
 		Temp->pNext = New;
 	}
+	// удаление первого элемента
+	void pop_front() {
+		Element* Temp = Head;
+		if (Temp->pNext) {
+			Temp = Temp->pNext;
+			Head = Temp;
+		}
+		else {
+			Head = nullptr;
+		}
+	}
+	// удаление последнего элемента
+	void pop_back() {
+		Element* Temp = Head;
+		Element* TempStep = Temp->pNext;
+		while (TempStep->pNext) {
+			TempStep = TempStep->pNext;
+			Temp = Temp->pNext;
+		}
+		Temp->pNext = nullptr;
+	}
+	// вставить значение по индексу
+	bool insert(int Data, int index) {
+		Element* New = new Element(Data);				// создадим элемент
+		Element* Temp = Head;							// запишим начальный адрес lista
+		Element* TempStep = Temp->pNext;				// запишим адрес следующего элемента lista
+		for (int i = 0; i < index - 1; i++) {			// перейдем к указанному индексу
+			if (TempStep == nullptr) return false;		// 
+			Temp = Temp->pNext;
+			TempStep = TempStep->pNext;
+		}
+		New->pNext = TempStep;
+		Temp->pNext = New;
+
+		return true;
+	}
+	// удаление элемента по индексу
+	bool erase(int index) {
+		Element* Temp = Head;
+		Element* TempStep = Temp->pNext;
+		if (index == 0) {
+			Head = TempStep;
+		}
+		else {
+			for (int i = 0; i < index - 1; i++) {			// перейдем к указанному индексу
+				if (TempStep == nullptr) return false;		// 
+				Temp = Temp->pNext;
+				TempStep = TempStep->pNext;
+			}
+			if (TempStep != nullptr) {
+				Temp->pNext = TempStep->pNext;
+				Temp = TempStep;
+			}
+			else {
+				Temp->pNext = nullptr; 
+				return false;
+			}
+		}
+		return true;
+	}
 
 	void print() {
 		Element* Temp = Head;					// Temp - это итератор (указатель при помощи которого можно
@@ -71,5 +131,25 @@ void main() {
 		//list.push_front(rand() % 100);
 	}	
 	list.push_back(123);
+	list.print();
+	cout << endl;
+
+	list.pop_front();
+	list.print();
+	cout << endl;
+
+	list.pop_back();
+	list.print();
+	cout << endl;
+
+	if (list.insert(28, 4) == false) {
+		cout << tab << "ОШИБКА ВСТАВКИ ЭЛЕМЕНТА. НАХОДИТСЯ ЗА ПРЕДЕЛАМИ lista" << endl;
+	}
+	list.print();
+	cout << endl;
+
+	if (list.erase(0) == false) {
+		cout << tab << "ОШИБКА УДАЛЕНИЯ ЭЛЕМЕНТА. НАХОДИТСЯ ЗА ПРЕДЕЛАМИ lista" << endl;
+	}
 	list.print();
 }
