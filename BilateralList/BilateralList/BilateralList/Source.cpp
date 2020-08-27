6e4fc0b4b1cefc3f49f45c867a108c8ef783513e
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-08-27 19:45:42 +0300

    New

--- BilateralList/BilateralList/BilateralList/Source.cpp
+++ BilateralList/BilateralList/BilateralList/Source.cpp
@@ -12,7 +12,24 @@ public:
 	~List() {
 		cout << "LDestructor:\t" << this << endl;
 	}
-
+	//---------------------------------------------------------------------
+	// Method
+	void push_front(int Data) {
+		/*Element* New = new Element(Data);
+		New->pNext = Head;
+		if (Head == nullptr) {
+			Tail = New;
+		}
+		else Head->pPrev = New;
+		Head = New;*/
+		if (Head == nullptr && Tail == nullptr) {
+		Head = Tail = new Element(Data, Head);
+		return;
+		}
+		Head = Head->pPrev = new Element(Data, Head);
+		size++;
+	}
+	//_____________________________________________________________________
 private:
 	class Element {
 
@@ -23,6 +40,7 @@ private:
 		~Element() {
 			cout << "EDestructor:\t" << this << endl;
 		}
+		friend class List;
 	private:
 		int Data;
 		Element* pNext;
@@ -33,14 +51,12 @@ private:
 	int size;
 };
 
-List::List() {
-}
-
-List::~List() {
-}
 
 void main() {
 	setlocale(LC_ALL, "");
 	List list;
+	list.push_front(5);
+	list.push_front(3);
+	list.push_front(4);
 
 }
